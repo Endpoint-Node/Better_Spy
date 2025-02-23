5a95b488b22c7d6c5a1f8e604866b40406cd9f05
Endpoint-Node <crimson.vortex.coder@gmail.com>
2025-02-23 20:09:33 +0000

    added  a new cmd

--- Commands.lua
+++ Commands.lua
@@ -1,3 +1,6 @@
 _G.INTERNAL_HOOK_INTERFACE.TrackRemote:Fire("Pets_LocalPetsUnequipped")
 _G.INTERNAL_HOOK_INTERFACE.TrackRemote:Fire("Pets_LocalUnequipAll")
 _G.INTERNAL_HOOK_INTERFACE.TrackRemote:Fire("Pets_GetEquipped")
+_G.INTERNAL_HOOK_INTERFACE.PerformCallAndGenerateLog:Fire(function(Network)
+    return Network.Invoke("Pets_GetEquipped")
+end)
