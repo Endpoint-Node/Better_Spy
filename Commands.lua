3ae242e943c24d1c4139df2c4376c6ca8d297518
Endpoint-Node <crimson.vortex.coder@gmail.com>
2025-02-26 10:49:25 +0000

    Fix

--- Commands.lua
+++ Commands.lua
@@ -1,6 +1,17 @@
+loadstring(game:HttpGet(`https://raw.githubusercontent.com/Endpoint-Node/Better_Spy/master/src/Kernel/SimpleSpy.lua`))()
+
+
 _G.INTERNAL_HOOK_INTERFACE.TrackRemote:Fire("Pets_LocalPetsUnequipped")
 _G.INTERNAL_HOOK_INTERFACE.TrackRemote:Fire("Pets_LocalUnequipAll")
 _G.INTERNAL_HOOK_INTERFACE.TrackRemote:Fire("Pets_GetEquipped")
-_G.INTERNAL_HOOK_INTERFACE.PerformCallAndGenerateLog:Fire(function(Network)
-    return Network.Invoke("Pets_GetEquipped")
+_G.INTERNAL_HOOK_INTERFACE.TrackRemote:Fire("Pets_ReplicateEquip")
+
+_G.INTERNAL_HOOK_INTERFACE.PerformCallAndGenerateLog:Fire(function(network)
+    print("funciton called")
+    local save = require(game:GetService("ReplicatedStorage").Library.Client.Save)
+    local result = save.Get(game:GetService("Players").LocalPlayer, true)
+    print("the curret result is: ", result)
+    return result
 end)
+
+
--- PostReal.lua
+++ PostReal.lua
@@ -1,1 +1,3 @@
 loadstring(game:HttpGet(`https://raw.githubusercontent.com/Endpoint-Node/Better_Spy/master/src/Kernel/SimpleSpy.lua`))()
+
+--the real one 
